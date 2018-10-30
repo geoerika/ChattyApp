4c98bd751e2425d5d23ddb9a6308d8224ec287b6
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-30 17:53:51 +0000

    added componentDidMount()

--- src/App.jsx
+++ src/App.jsx
@@ -43,12 +43,12 @@ import ChatBar from './ChatBar.jsx';
 const currentUser = {name: "Bob"};// optional. if currentUser is not defined, it means the user is Anonymous
 const messages = [
     {
-      id: '1',
+      id: 1,
       username: "Bob",
       content: "Has anyone seen my marbles?",
     },
     {
-      id: '2',
+      id: 2,
       username: "Anonymous",
       content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
     }
@@ -67,6 +67,21 @@ class App extends Component {
     };
     console.log('currentUser: ', this.state.currentUser);
     console.log('messages', this.state.messages);
+  }
+
+  componentDidMount() {
+
+    console.log("componentDidMount <App />");
+
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+     // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage);
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages});
+    }, 3000);
   }
 
 
