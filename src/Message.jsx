477141620a856cfd26d903a483ba51bce09c507c
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-30 13:03:18 +0000

    split the html into differentjsx components

---
+++ src/Message.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+
+  render() {
+
+    return (
+
+      <main className="messages">
+
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+
+        <div className="message system">
+        Anonymous1 changed their name to nomnom.
+        </div>
+
+      </main>
+    );
+  }
+}
+
+export default Message;
