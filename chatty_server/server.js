632c7eb8dd0dae1a8f647f4e0e19ebc72b4c8de6
Erika Szabo <erika.szabo.geo@gmail.com>
2018-10-31 16:14:41 +0000

    created a chatty_server which receives messages from a client

---
+++ chatty_server/server.js
@@ -0,0 +1,35 @@
+// server.js
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+let messageReceived = {};
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', function incoming(message) {
+    messageReceived = JSON.parse(message);
+    console.log('User ' + messageReceived.username + ' said ' + messageReceived.content);
+  });
+
+  ws.send('something');
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+
