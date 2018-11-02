670f4886123c36335e910fed62c6b17495867977
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-02 02:34:26 +0000

    passed on clientColour={message.clientColour} to Message component

--- client/src/MessageList.jsx
+++ client/src/MessageList.jsx
@@ -8,8 +8,9 @@ class MessageList extends Component {
   render() {
 
     console.log('Messages in messageList: ', this.props.messages);
+    console.log('this.props.clientColour in MessageList: ', this.props.clientColour);
     const messageList = this.props.messages.map(message => (
-      <Message key={message.id} messageUser={message.username} messageContent={message.content} messageType={message.type}/>
+      <Message key={message.id} clientColour={message.clientColour} messageUser={message.username} messageContent={message.content} messageType={message.type}/>
     ));
 
     return (
