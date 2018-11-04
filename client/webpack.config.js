9b71ce76920726ab4a8341ebca7a87d935c0aa91
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-04 02:59:12 +0000

    included test for url-loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,17 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   }
