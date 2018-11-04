2bb3dffde1cd3980d360cda172dc4bb082e16913
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-04 03:05:47 +0000

    cleaned code and inserted comments

--- client/src/MessageList.jsx
+++ client/src/MessageList.jsx
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
 
-
-
+//component which receives the list of messages and maps them to Message component for displaying
 class MessageList extends Component {
 
   render() {
-
-    console.log('Messages in messageList: ', this.props.messages);
-    console.log('this.props.clientColour in MessageList: ', this.props.clientColour);
+    //mapping messages in the database to send them to the Message component with apropriate props
     const messageList = this.props.messages.map(message => (
-      <Message key={message.id} clientColour={message.clientColour} messageUser={message.username} messageContent={message.content} messageType={message.type}/>
+
+      <Message key={message.id} clientColour={message.clientColour} messageUser={message.username}
+               messageContent={message.content} messageType={message.type}/>
     ));
 
     return (
-
+      //displays all the messages, notifications, and images
       <main className="messages">
         {messageList}
       </main>
