997c0a822c6496a4fa73464c2042761197a04e79
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-04 03:07:00 +0000

    added component for displaying image, cleaned code and inserted comments

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -1,36 +1,49 @@
 import React, {Component} from 'react';
 
+//component which handles various type of messages and displays them accordingly
 class Message extends Component {
-
-
 
   render() {
 
-    console.log('this.props in Message: ', this.props);
     let messageType = this.props.messageType;
-    console.log('messageType in Message: ', messageType);
-    console.log('this.props.clientColour in Chatbar: ', this.props.clientColour);
     let messageItem ='';
 
+    //case for incoming message
     if(messageType === 'incomingMessage') {
+
       messageItem = (
-          <div className="message">
-            <span className="message-username" style={{color: this.props.clientColour}}>{this.props.messageUser}</span>
-            <span className="message-content">{this.props.messageContent}</span>
-          </div>
+
+        <div className="message">
+          <span className="message-username" style={{color: this.props.clientColour}}>{this.props.messageUser}</span>
+          <span className="message-content">{this.props.messageContent}</span>
+        </div>
       )
     }
 
+    //case for incoming notification
     if (messageType === 'incomingNotification') {
 
       messageItem = (
-            <div className="notification">
-              <span className="notification-content">{this.props.messageContent}</span>
-            </div>
-        )
+
+        <div className="notification">
+          <span className="notification-content">{this.props.messageContent}</span>
+        </div>
+      )
     }
 
-    console.log('messageItem: ', messageItem);
+    //case for incoming image
+    if (messageType === 'image') {
+
+      messageItem = (
+
+        <div className="image">
+          <span className="message-username" style={{color: this.props.clientColour}}>{this.props.messageUser}</span>
+          <div>
+            <img className="message-image" src={this.props.messageContent} width={'20%'}/>
+          </div>
+        </div>
+      )
+    }
 
     return (
       <div>
